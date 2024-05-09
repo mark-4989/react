6341a0c7d1f52eeb04f3b5b3a8ff49e342ef260e
mark <ngarimark27@gmail.com>
2024-05-09 12:36:24 +0300

    created navbar without style

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/Navbar.js";
+import Navbar from "./components/Navbar.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#"> Home </a>{" "}
+          </li>{" "}
+          <li>
+            <a href=""> Create </a>{" "}
+          </li>{" "}
+        </ul>{" "}
+      </nav>{" "}
+    </div>
+  );
+};
+export default Navbar;
