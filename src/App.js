58e70588b4119a38cb391f238735d27dba0e32bd
mark <ngarimark27@gmail.com>
2024-05-13 23:05:02 +0300

    styled navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./components/Navbar.js";
 import Navbar from "./components/Navbar.js";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from "react-router-dom/cjs/react-router-dom.min.js";
+import "./components/Home.js";
+import Home from "./components/Home.js";
+import "./components/Create.js";
+import Create from "./components/Create.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+    return ( <
+        Router >
+        <
+        div className = "App" >
+        <
+        Navbar / >
+        <
+        div className = "content" >
+        <
+        Switch >
+        <
+        Route exact path = "/" >
+        <
+        Home / >
+        <
+        /Route>{" "} <
+        Route path = "/Create" >
+        <
+        Create / >
+        <
+        /Route>{" "} <
+        /Switch>{" "} <
+        /div>{" "} <
+        /div>{" "} <
+        /Router>
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,15 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <a href="#"> Home </a>{" "}
-          </li>{" "}
-          <li>
-            <a href=""> Create </a>{" "}
-          </li>{" "}
-        </ul>{" "}
-      </nav>{" "}
-    </div>
-  );
+    return ( <
+        nav className = "navbar" >
+        <
+        h2 > Blog Post < /h2> <
+        div className = "links" >
+        <
+        Link to = "/" > Home < /Link> <
+        Link to = "/Create" > new Blog < /Link> <
+        /div>{" "} <
+        /nav>
+    );
 };
 export default Navbar;
